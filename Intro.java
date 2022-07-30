c42a6172d243df26ff18728a6a42f407f6608af6
Yeshua Lopez <lopezyeshua@gmail.com>
2022-07-29 21:58:00 -0700

    first commit

---
+++ Intro.java
@@ -0,0 +1,5 @@
+public class Intro {
+    public static void main(String[] args) {
+        System.out.println("My name is Yeshua Lopez\nI am 100 years old\nMy hometown is North Hollywood, CA");
+    }
+}
